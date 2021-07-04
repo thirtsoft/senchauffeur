ef4f3085ccf1669a084fa9c505b29309630d4671
thirdiallo <thirdiallo@gmail.com>
2021-07-04 14:50:45 +0000

    change double cote to simple cote in server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist/senchauffeur"));
+app.use(express.static(__dirname + '/dist/senchauffeur'));
 
 app.listen(process.env.PORT || 4200);
 
-app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname + "/dist/senchauffeur/index.html"));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/senchauffeur/index.html'));
 });
 
 console.log("App is listenning!");
