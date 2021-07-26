5298313e32665d40235e5c0b11a8b8cbb7df08a3
thirdiallo <thirdiallo@gmail.com>
2021-07-26 00:02:19 +0000

    Add notification and recruteur in dashboard

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,3 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { ChauffeurDto } from './../../models/chauffeur';
+import { RecruteurDto } from './../../models/recruteur';
+import { NotationDto } from './../../models/notation';
+import { RecruteurService } from './../../services/recruteur.service';
+import { NotationService } from './../../services/notation.service';
 import { Router } from '@angular/router';
 import { ChauffeurService } from './../../services/chauffeur.service';
 import { AnnonceService } from './../../services/annonce.service';
@@ -11,11 +17,18 @@ import { DashboardService } from 'src/app/services/dashboard.service';
 })
 export class DashboardComponent implements OnInit {
 
+  listNotificationDTO: NotationDto[];
+  listRecruteurDTO: RecruteurDto[];
+  listChauffeurDTO: ChauffeurDto[];
+
   numberOfChauffeur;
   numberOfAnnonce;
   numberOfRecruteur;
 
   constructor(public dasboardService: DashboardService,
+              private noteService: NotationService,
+              private chauffService: ChauffeurService,
+              private recService: RecruteurService,
               private router: Router,
 
   ) {}
@@ -27,6 +40,10 @@ export class DashboardComponent implements OnInit {
     this.getNumberOfRecruteurs();
 
     this.getNumberOfAnnonces();
+
+    this.getListNotationDtos();
+
+    this.getListRecruteurDTO();
   }
 
   getNumberOfChauffeurs(): void {
@@ -45,6 +62,32 @@ export class DashboardComponent implements OnInit {
     this.dasboardService.countNumberOfRecruteurs().subscribe(data => {
       this.numberOfRecruteur = data;
     });
+  }
+
+  public getListNotationDtos() {
+    this.noteService.getNotationDTOs().subscribe(
+      (response: NotationDto[]) => {
+        this.listNotificationDTO = response;
+        console.log(this.listNotificationDTO);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+
+  }
+
+  public getListRecruteurDTO() {
+    this.recService.getRecruteurDTOs().subscribe(
+      (response: RecruteurDto[]) => {
+        this.listRecruteurDTO = response;
+        console.log(this.listRecruteurDTO);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+
   }
 
 
