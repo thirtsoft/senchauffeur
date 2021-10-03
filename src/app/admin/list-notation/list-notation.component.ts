72f719253ce699351faa23b9d65341bcd8eff974
thirtsoft <thirtsoft@gmail.com>
2021-10-03 03:57:42 +0000

    remove field in rating entitie

--- src/app/admin/list-notation/list-notation.component.ts
+++ src/app/admin/list-notation/list-notation.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { DialogService } from './../../services/dialog.service';
-import { ToastrService } from 'ngx-toastr';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { NotationService } from './../../services/notation.service';
